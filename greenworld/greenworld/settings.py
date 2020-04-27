287cfacc79a6245a9b34d92b0eb1cb285a073a48
Xander Dean <aaxelld@gmail.com>
2020-04-27 03:20:54 -0400

    re-added bootstrap

--- greenworld/greenworld/settings.py
+++ greenworld/greenworld/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'gwgarden/build/static'),
 # ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
