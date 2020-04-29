90c0d4e6bbe51182a3e5b8085c40de003684dbe3
Xander Dean <aaxelld@gmail.com>
2020-04-28 22:13:51 -0400

    setting up redux

---
+++ greenworld/frontend/src/actions/stores.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+import { GET_STORES } from './types';
+
+// GET STORES
+export const getStores
---
+++ greenworld/frontend/src/components/stores/Stores.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Stores extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Stores List</h1>
+            </div>
+        )
+    }
+}
+
+export default Stores
