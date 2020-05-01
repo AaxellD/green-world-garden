c0b9b14e234b44cb008eac8d783d7b626f5d429c
Xander Dean <aaxelld@gmail.com>
2020-04-30 22:17:19 -0400

    changed static root folder

--- greenworld_app/greenworld/settings.py
+++ greenworld_app/greenworld/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static')
+]
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
