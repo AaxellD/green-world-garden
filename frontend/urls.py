dcd2b5d00038346c66b0592d4b8c4335ed5a70ce
Xander Dean <aaxelld@gmail.com>
2020-05-10 19:47:10 -0400

    added static to urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index)
-]
-#  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
